58d7158dd4af0f38fcd53238a21aa569db45a294
Alex Johansson <alexander@n1s.se>
2022-07-07 09:11:32 +0200

    add todomvc (#2116)

--- packages/react/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@trpc/client';
-
-export { createReactQueryHooks } from './createReactQueryHooks';
-export { createReactQueryProxy } from './createReactQueryProxy';
--- packages/server/src/core/index.ts
+++
@@ -1,5 +0,0 @@
-export type { AnyRouter, ProcedureRouterRecord } from './router';
-export type { Procedure, ProcedureParams } from './procedure';
-
-export { initTRPC } from './initTRPC';
-export * from './types';
